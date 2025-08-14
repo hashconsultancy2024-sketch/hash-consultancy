b07cfd5bdba7f86ac822cd1b9f8abbef191ee313
hashconsultancy2024-sketch <hash.consultancy2024@gmail.com>
2025-08-15 04:11:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const msg = document.getElementById('formMessage');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  msg.classList.remove('hidden');
-  form.reset();
+  fetch(form.action, {
+    method: 'POST',
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+  })
+  .then(response => {
+    if (response.ok) {
+      msg.classList.remove('hidden');
+      form.reset();
+    } else {
+      alert('Oops! Something went wrong. Please try again.');
+    }
+  })
+  .catch(() => alert('Oops! Could not send your inquiry.'));
 });
